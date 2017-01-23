fddd3d3f6730a5ac90a2a68aec7a514d44c080f9
antjohan <antjohan@student.chalmers.se>
2017-01-23 21:01:01 +0100

    v0.11

--- src/cfs/dashboard/DashboardController.java
+++ src/cfs/dashboard/DashboardController.java
@@ -5,14 +5,12 @@
  */
 package cfs.dashboard;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-
 
 public class DashboardController implements Initializable {
 
@@ -21,49 +19,56 @@ public class DashboardController implements Initializable {
      */
     @FXML
     private Button Start;
-    
+
     @FXML
     private Button Stop;
     @FXML
     private Label Output;
-    
+
     @FXML
     private Label ThreadStat;
     Read_UART UART = new Read_UART();
     Thread UART_Thread = new Thread(UART);
-    boolean ThreadRunning = false;  
+    boolean ThreadRunning = false;
     boolean ThreadSuspended = false;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
 
     }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         Start.setOnAction((event) -> {
-        if(!ThreadRunning){
-        UART_Thread.start();
-        ThreadRunning = true;
-        }else{
-        UART_Thread.resume();
-        }
-        Stop.setText("Suspend");
-        ThreadSuspended = true;
-        ThreadStat.setText("Runnig");
-        });
-        
-        Stop.setOnAction((event) -> {
-            if(ThreadSuspended){
-            UART_Thread.suspend();
-            Stop.setText("Stop");
+            if (!ThreadRunning) {
+                UART_Thread.start();
+                ThreadRunning = true;
+            }
+            Read_UART.Resume();
+            ThreadStat.setText("Runnig");
+            Stop.setText("Suspend");
+
             ThreadSuspended = false;
-            ThreadStat.setText("Suspended");
+
+        });
+
+        Stop.setOnAction((event) -> {
+            if (ThreadSuspended) {
+                //UART_Thread.interrupt();
+                Read_UART.Exit();
+                ThreadStat.setText("closed");
+
+            } else {
+                Read_UART.Stop();
+                ThreadSuspended = true;
+                ThreadStat.setText("Suspended");
+                Stop.setText("Stop");
+
             }
-            
-            if(!ThreadSuspended){
-                UART_Thread.stop();
-            ThreadStat.setText("Stoped");
-            }
+            //UART_Thread.interrupt();
+            System.out.println("1");
+
         });
     }
 
--- src/cfs/dashboard/Read_UART.java
+++ src/cfs/dashboard/Read_UART.java
@@ -7,30 +7,63 @@ package cfs.dashboard;
 
 import com.fazecast.jSerialComm.SerialPort;
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Anton
  */
 public class Read_UART implements Runnable {
+
     public void run() {
         Read();
     }
-    static void Read() {				
-		SerialPort port = SerialPort.getCommPort("/dev/cu.usbmodem1411");
-		port.openPort();
-		port.openPort();
-		port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
-		InputStream in = port.getInputStream();
-		try
-		{
-			while(true){
-				System.out.print((char)in.read());
-				if(false)
-					break;
-			}
-			in.close();
-		} catch (Exception e) { e.printStackTrace(); }
-		port.closePort();
-	}
+    static private boolean stop = false;
+    static private boolean exit = false;
+
+    static void Read() {
+        SerialPort port = SerialPort.getCommPort("/dev/cu.usbmodem1411");
+        while (true) {
+
+            if (!stop) {
+                port.openPort();
+                port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
+                InputStream in = port.getInputStream();
+                try {
+                    while (true) {
+                        System.out.print((char) in.read());
+                        if (stop) {
+                            break;
+                        }
+                    }
+                    in.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                port.closePort();
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Read_UART.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            if (exit) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+
+    static void Stop() {
+        stop = true;
+    }
+
+    static void Resume() {
+        stop = false;
+    }
+
+    static void Exit() {
+        exit = true;
+    }
 }
